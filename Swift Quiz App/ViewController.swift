e4f1de72331e442016ae223637b98aded230acb3
Ball Alexander <aball@grcmacm15smini2.tech.div>
2016-05-19 11:58:35 -0700

    Created Simple Hello World throwaway to learn the code and editor. Recreated project as a single-view app in order to make it easier to understand as I am just learning Xcode.

--- Swift Quiz App/ViewController.swift
+++ Swift Quiz App/ViewController.swift
@@ -19,6 +19,12 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func showMessage() {
+        let alertController = UIAlertController(title: "Welcome to My First App", message: "Hello World", preferredStyle: UIAlertControllerStyle.Alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+        self.presentViewController(alertController, animated: true, completion: nil)
+    }
 
 
 }
